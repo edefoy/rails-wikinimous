eed9f7c7afcb23b818af6b0343a47cea37ae8e1a
Erwann Defoy <ecdefoy1@gmail.com>
2025-02-25 15:58:20 +0100

    smthg

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |_|
+  article = Article.new(
+    title: Faker::GreekPhilosophers.name,
+    content: Faker::GreekPhilosophers.quote
+  )
+  article.save
+end
